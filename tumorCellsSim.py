5a12c829051b93623565152aee0f51d439f86cd4
Petter Säterskog <petter.saterskog@scilifelab.se>
2021-07-06 18:45:23 +0200

    removed neighbor interactions, fixed swap bug

--- tumorCellsSim.py
+++ tumorCellsSim.py
@@ -5,14 +5,21 @@ from scipy import signal
 import matplotlib.animation as animation
 from scipy.spatial import cKDTree
 #micrometers
-L = 2000
+L = 4000
 
-dt = 0.6 #hours
+dt = 0.1 #hours
+cellRad = 10 #this assumes no gap, just defined through volPerCell
+volPerCell = cellRad**2*np.pi
+immuneFraction = 0.01
+
+nCells = L**2 / volPerCell
 
 types = ['healthy', 'cancer', 'immune']
 colors = {'healthy':(0,1,0), 'cancer':(1,0,0), 'immune':(0,0,1)}
-cells = {'healthy':np.random.rand(7000,2)*L, 'cancer':np.array([[L/2, L/2]]), 'immune':np.random.rand(150,2)*L,}
+cells = {'healthy':np.random.rand(int(nCells*(1-immuneFraction)),2)*L, 'cancer':np.array([[L/2, L/2]]), 'immune':np.random.rand(int(nCells*immuneFraction),2)*L,}
+switchMat = {('cancer', 'immune'):0.8, ('cancer', 'healthy'):0.0, ('healthy','immune'):1.5}
 
+frames = 0
 def updatefig(*args):
 	for t in cells:
 		inside =  (cells[t][:,0] > 0) & (cells[t][:,0] < L) & (cells[t][:,1] > 0) & (cells[t][:,1] < L)
@@ -24,33 +31,38 @@ def updatefig(*args):
 	rad = 30
 	displacements = {t: np.zeros(cells[t].shape) for t in cells}
 
-	switchMat = {('cancer', 'immune'):0.2, ('cancer', 'healthy'):0.2, ('healthy','immune'):1.5}
 	pairs={}
 	for t1 in cells:
 		for t2 in cells:
 			pairs[(t1,t2)] = trees[t1].query_ball_tree(trees[t2], rad)
-			for i in range(len(pairs[(t1,t2)])):
-				c = cells[t1][i, :]
-				for o in pairs[(t1,t2)][i]:
-					diff = c-cells[t2][o,:]
-					n = np.sum(diff**2)
-					if n>0:
-						displacements[t1][i,:] += 5.*diff/n
+			# for i in range(len(pairs[(t1,t2)])):
+			# 	c = cells[t1][i, :]
+			# 	for o in pairs[(t1,t2)][i]:
+			# 		diff = c-cells[t2][o,:]
+			# 		n = np.sum(diff**2)
+			# 		if n>0:
+			# 			displacements[t1][i,:] += 5.*diff/n
 
 	for t1 in cells:
 		for t2 in cells:
-			pairList = np.array( [(i,j) for i,ps in enumerate(pairs[(t1,t2)]) for j in ps ] )
-			
 			if t1<t2:
-				if pairList.shape[0]:
-					sPairs = pairList[ np.random.rand(pairList.shape[0])<dt*switchMat[(t1,t2)], : ]
+				ps = pairs[(t1,t2)]
+				for i in range(len(ps)):
+					if np.random.rand() < dt*switchMat[(t1,t2)]*len(ps[i]):
+						j = np.random.randint(len(ps[i]))
+						cells[t1][i, :], cells[t2][ps[i][j], :] = np.array(cells[t2][ps[i][j], :]), np.array(cells[t1][i, :])
+			# pairList = np.array( [(i,j) for i,ps in enumerate(pairs[(t1,t2)]) for j in ps ] )
+			# switchCell1 = np.arange(len(cells))[]
+			# if t1<t2:
+			# 	if pairList.shape[0]:
+			# 		sPairs = pairList[ np.random.rand(pairList.shape[0])<dt*switchMat[(t1,t2)], : ]
 					
-					cells[t1][sPairs[:,0],:], cells[t2][sPairs[:,1],:] = cells[t2][sPairs[:,1],:], cells[t1][sPairs[:,0],:]
-					displacements[t1][sPairs[:,0],:], displacements[t2][sPairs[:,1],:] = displacements[t2][sPairs[:,1],:], displacements[t1][sPairs[:,0],:]
+			# 		cells[t1][sPairs[:,0],:], cells[t2][sPairs[:,1],:] = cells[t2][sPairs[:,1],:], cells[t1][sPairs[:,0],:]
+					# displacements[t1][sPairs[:,0],:], displacements[t2][sPairs[:,1],:] = displacements[t2][sPairs[:,1],:], displacements[t1][sPairs[:,0],:]
 				
 			#pick some switches..
 
-
+	splitRad = cellRad/2
 
 	for t1 in cells:
 		if t1 == 'healthy':
@@ -60,8 +72,8 @@ def updatefig(*args):
 			if t1=='cancer':
 				growth = 0.1
 			elif t1=='immune':
-				if len(trees['cancer'].query_ball_point(cc, 30))>0:
-					growth = 0.2
+				if len(trees['cancer'].query_ball_point(cc, 3*cellRad))>0:
+					growth = 0.11
 				else:
 					growth = 0
 			if np.random.rand()<dt*growth:
@@ -70,29 +82,43 @@ def updatefig(*args):
 					diffs = cells[t2] - cc
 					ns = np.sum(diffs**2, axis=1)
 					
-					displacements[t2][ns>0,:] += 100 * diffs[ns>0,:] / ns[ns>0][:,np.newaxis]
+					displacements[t2][ns>0,:] += diffs[ns>0,:] * (np.sqrt(cellRad**2/ns[ns>0] + 1) - 1)[:,np.newaxis] # diffs[ns>0,:] / ns[ns>0][:,np.newaxis]
 			
 	for t in cells:
 		cells[t] += displacements[t]
-		
+	
+	
 	for t in cells:
 		randDirs = np.random.normal(size=(len(newCells[t]),2))
-		randDirs = 0.5*rad*randDirs/np.linalg.norm(randDirs, axis=1)[:, np.newaxis]
+		randDirs = 0.5*splitRad*randDirs/np.linalg.norm(randDirs, axis=1)[:, np.newaxis]
 		cells[t] = np.concatenate((cells[t], cells[t][newCells[t]] + randDirs), axis=0)
 		cells[t][newCells[t]] -= randDirs
 	
 	for t in cells:
 		sc[t].set_offsets(cells[t])
-			
+	s=""
+	for t in cells:
+		s+=f"{t}: {len(cells[t])}, "
+
+	global frames
+	print(f"frame {frames}: {s[:-2]}")
+	frames += 1
 	return sc.values()
 
-fig = pl.figure(figsize=(8,8))
+fig = pl.figure(figsize=(12,12))
 sc={}
 for t in cells:
-	sc[t] = pl.scatter(cells[t][:,0],cells[t][:,1], s=1, color=colors[t])
+	sc[t] = pl.scatter(cells[t][:,0],cells[t][:,1], s=2, color=colors[t])
 pl.xlabel("x [μm]")
 pl.ylabel("y [μm]")
 
-ani = animation.FuncAnimation(fig, updatefig, interval=10)#, blit=True)
+anim = animation.FuncAnimation(fig, updatefig, interval=10, blit=True, frames=100000)
+writervideo = animation.FFMpegWriter(fps=30) 
 
-pl.show()
+outFolder = "out/tumorSim"
+from pathlib import Path
+Path(outFolder).mkdir(parents=True, exist_ok=True)
+diff=str(list(switchMat.values()))
+anim.save(f"{outFolder}/tumor_L={L}_diffusion={diff[1:-1]}.avi", writer=writervideo)
+
+# pl.show()
